a54ef25892dad309114d6c537e1b955d2786663e
Ayushi Chaudhary <48730884+Ayuushii@users.noreply.github.com>
2025-12-10 01:07:24 -0600

    Add Solution: Selection Sort

---
+++ gfg-selection-sort/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    // Function to perform selection sort on the given array.
+    void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+    void selectionSort(vector<int> &arr) {
+        // code here
+        
+    }
+};
