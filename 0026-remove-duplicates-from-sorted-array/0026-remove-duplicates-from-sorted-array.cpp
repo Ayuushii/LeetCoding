1950793d4a39c1d543f4c85952bbc9050fa4dba8
Ayushi Chaudhary <48730884+Ayuushii@users.noreply.github.com>
2025-12-30 00:07:26 -0600

    Time: 0 ms (100%), Space: 22.8 MB (10.14%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-       //Keep a write index that tracks the correct writes to their positions;
-       int i=0;
-       //Keep a read index that moves till we find a number different than current
-       int j=0;
-       //A variable to store what value we already have and dont need anymore further.
-       int prev=-101;
-       for(i=0; i<nums.size();i++) {
-        //If we found a new value
-        if(nums[i]!=prev){
-            nums[j] = nums[i];
-            prev = nums[i];
-            j++;
+    //    //Keep a write index that tracks the correct writes to their positions;
+    //    int i=0;
+    //    //Keep a read index that moves till we find a number different than current
+    //    int j=0;
+    //    //A variable to store what value we already have and dont need anymore further.
+    //    int prev=-101;
+    //    for(i=0; i<nums.size();i++) {
+    //     //If we found a new value
+    //     if(nums[i]!=prev){
+    //         nums[j] = nums[i];
+    //         prev = nums[i];
+    //         j++;
+    //     }
+    //    }
+    //    return j;
+
+    //Easier solution
+        int j = 1;
+        for(int i=1; i<nums.size();i++){
+            if(nums[i-1]!=nums[i]){
+                nums[j]=nums[i];
+                j++;
+            }
         }
-       }
-       return j;
+        return j;
     }
 };
