902670d5f0df55fab80348aaebce8abe6f0b7c0b
Ayushi Chaudhary <48730884+Ayuushii@users.noreply.github.com>
2026-01-14 01:02:17 -0600

    Time: 0 ms (100%), Space: 8.4 MB (54.19%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        int count=0;
+        for(char &ch: s) {
+            if(ch=='(') {
+                st.push(ch);
+            } else {
+                if(st.empty()){
+                    count++;
+                } else {
+                    st.pop();
+                }
+            }
+        }
+        return count+st.size();
+    }
+};
