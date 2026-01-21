38061b232586ac4a1cab96af45dff501b05a1472
Ayushi Chaudhary <48730884+Ayuushii@users.noreply.github.com>
2026-01-21 17:42:11 -0600

    Time: 0 ms (100%), Space: 14.9 MB (36.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //Use hashmap where element is the key and its value is the index.
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++) {
-            int x = nums[i];
-            int y = target-nums[i];
-            if(mp.find(y)!=mp.end())
-                return {mp[y],i};
-            mp[x] = i;
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        for(int i=0; i<n; i++) {
+            int complement = target-nums[i];
+            if(mp.find(complement)!=mp.end())
+                return {i, mp[complement]};
+            mp[nums[i]] = i; 
         }
         return {-1,-1};
     }
