675164b0465cde00a23146b8c92f4b80366c70c3
Ayushi Chaudhary <48730884+Ayuushii@users.noreply.github.com>
2026-02-10 23:13:55 -0600

    Time: 396 ms (5.21%), Space: 59.9 MB (7.05%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int count = 0;
+    void numperm(int pos, int n, vector<bool> used) {
+        if(pos>n) {
+            count++;
+            return;
+        }
+        for(int num=1; num<=n; num++) {
+            if(used[num]) continue;
+            if(num%pos==0 || pos%num==0) {
+                used[num] = true;
+                numperm(pos+1, n, used);
+                used[num] = false;
+            }
+        }
+    }
+
+
+    int countArrangement(int n) {
+        //Brute force - form all permutations - n! and then check
+
+        /*Backtrack - find correct number for that position using the rule 
+        and then mark it used and go doing the same thing till we fill all
+        positions and then we have found the permutation */
+        vector<bool> used(n+1, false);
+        numperm(1, n, used);
+        return count;
+    }
+};
